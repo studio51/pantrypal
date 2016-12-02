fffd43f43e65f867f59aa8f7ed23eb79ae9e62e1
pacMakaveli <pacMakaveli90@gmail.co.uk>
2016-12-02 08:05:47 +0000

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  goToPantry() {
+
+  }
+
+  addNewItemToThePantry() {
+
+  }
+
+  goToRecipes() {
+
+  }
+}
